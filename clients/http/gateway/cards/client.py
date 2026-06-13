7f27879bb3c7c99b2a69396fbb800b99183c3347
janshv <janshv.dmitry@mail.ru>
2026-06-13 13:29:04 +0300

    Практика использования HTTP API-клиентов: получение документов по счету

--- clients/http/gateway/cards/client.py
+++ clients/http/gateway/cards/client.py
@@ -4,12 +4,34 @@ from clients.http.client import HTTPClient
 from clients.http.gateway.client import build_gateway_http_client
 
 
+class CardDict(TypedDict):
+    """
+    Описание структуры карты.
+    """
+    id: str
+    pin: str
+    cvv: str
+    type: str
+    status: str
+    accountId: str
+    cardNumber: str
+    cardHolder: str
+    expiryDate: str
+    paymentSystem: str
+
+
 class IssueVirtualCardRequestDict(TypedDict):
     """
     Структура данных для выпуска виртуальной карты.
     """
     userId: str
     accountId: str
+
+class IssueVirtualCardResponseDict(TypedDict):
+    """
+    Описание структуры ответа выпуска виртуальной карты.
+    """
+    card: CardDict
 
 
 class IssuePhysicalCardRequestDict(TypedDict):
@@ -18,6 +40,12 @@ class IssuePhysicalCardRequestDict(TypedDict):
     """
     userId: str
     accountId: str
+
+class IssuePhysicalCardResponseDict(TypedDict):
+    """
+    Описание структуры ответа выпуска физической карты.
+    """
+    card: CardDict
 
 
 class CardsGatewayHTTPClient(HTTPClient):
@@ -42,6 +70,16 @@ class CardsGatewayHTTPClient(HTTPClient):
         :return: Ответ от сервера (объект httpx.Response).
         """
         return self.post("/api/v1/cards/issue-physical-card", json=request)
+
+    def issue_virtual_card(self, user_id: str, account_id: str) -> IssueVirtualCardResponseDict:
+        request = IssueVirtualCardRequestDict(userId=user_id, accountId=account_id)
+        response = self.issue_virtual_card_api(request)
+        return response.json()
+
+    def issue_physical_card(self, user_id: str, account_id: str) -> IssuePhysicalCardResponseDict:
+        request = IssuePhysicalCardRequestDict(userId=user_id, accountId=account_id)
+        response = self.issue_physical_card_api(request)
+        return response.json()
 
 
 def build_cards_gateway_http_client() -> CardsGatewayHTTPClient:
