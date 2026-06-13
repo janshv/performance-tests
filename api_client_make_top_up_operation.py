49372f8619277b748d5fc13b955b20d1b72510ff
janshv <janshv.dmitry@mail.ru>
2026-06-13 14:57:42 +0300

    Практика использования HTTP API-клиентов: создание операции пополнения счета

--- api_client_make_top_up_operation.py
+++ api_client_make_top_up_operation.py
@@ -8,12 +8,12 @@ accounts_gateway_client = build_accounts_gateway_http_client()
 operations_gateway_client = build_operations_gateway_http_client()
 
 
-# Создаем пользователя
+# Создание пользователя
 create_user_response = users_gateway_client.create_user()
 print('Create user response:', create_user_response)
 
 
-# Открываем кредитный счет
+# Открытие дебетового счета
 open_debit_card_account_response = accounts_gateway_client.open_debit_card_account(
     user_id=create_user_response['user']['id']
 )
