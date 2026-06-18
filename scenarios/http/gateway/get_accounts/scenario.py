22fe647d2cb98cafd8eb367c51d48a30146596c9
janshv <janshv.dmitry@mail.ru>
2026-06-18 13:18:08 +0300

    Практика: работа с настройками Locust

---
+++ scenarios/http/gateway/get_accounts/scenario.py
@@ -0,0 +1,56 @@
+from locust import User, between, task
+from clients.http.gateway.locust import GatewayHTTPTaskSet
+from clients.http.gateway.users.schema import CreateUserResponseSchema
+
+
+class GetAccountsTaskSet(GatewayHTTPTaskSet):
+    """
+    Нагрузочный сценарий, который последовательно:
+    1. Создаёт нового пользователя.
+    2. Открывает депозитный счёт.
+    3. Получает список всех счетов пользователя.
+
+    Использует базовый GatewayHTTPTaskSet и уже созданных в нём API клиентов.
+    """
+
+    # Shared state — сохраняем результаты запросов для дальнейшего использования
+    create_user_response: CreateUserResponseSchema | None = None
+
+    @task(2)
+    def create_user(self):
+        """
+        Создаём нового пользователя и сохраняем результат для последующих шагов.
+        """
+        self.create_user_response = self.users_gateway_client.create_user()
+
+    @task(2)
+    def open_savings_account(self):
+        """
+        Открываем депозитный счёт для созданного пользователя.
+        Проверяем, что предыдущий шаг был успешным.
+        """
+        if not self.create_user_response:
+            return  # Если пользователь не был создан, нет смысла продолжать
+
+        self.accounts_gateway_client.open_deposit_account(user_id=self.create_user_response.user.id)
+
+    @task(6)
+    def get_accounts(self):
+        """
+        Получаем данные о счетах пользователя.
+        """
+        if not self.create_user_response:
+            return  # Если пользователь не был создан, нет смысла продолжать
+
+        self.accounts_gateway_client.get_accounts(self.create_user_response.user.id)
+
+
+class GetAccountsScenarioUser(User):
+    """
+    Пользователь Locust, исполняющий последовательный сценарий получения документов.
+    """
+    host = "localhost"
+    tasks = [GetAccountsTaskSet]
+    wait_time = between(1, 3)  # Имитируем паузы между выполнением сценариев
+
+
