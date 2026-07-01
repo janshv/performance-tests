95b1b82eed53f2aa87d3f301e6556c77ed89d358
janshv <janshv.dmitry@mail.ru>
2026-07-01 13:29:52 +0300

    Fin Project

--- docker_compose_example.py
+++
@@ -1,6 +0,0 @@
-from redis import Redis
-
-cache = Redis(host="redis", port=6379)
-cache.set("example", 5)
-cache.incr("times", 1)
-print("SQRT: ", int(cache.get("example")) ** 2, "Incr: ", cache.get('times'))
